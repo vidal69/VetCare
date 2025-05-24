06d88f8a2c8a17dafd04cee0c3dfabf7d0c56c0d
Ajman Mocsana <ajman.mocsana@g.msuiit.edu.ph>
2025-05-24 21:42:40 +0800

    retained trimming on certain txts

--- src/app/PatientManager.java
+++ src/app/PatientManager.java
@@ -295,11 +295,11 @@ public class PatientManager extends JPanel {
             String idText = txtID.getText().trim();
             String nameText = txtName.getText().trim();
             String dobText = txtDOB.getText().trim();
-            String genderText = (String) cbGender.getSelectedItem();
+            String genderText = ((String) cbGender.getSelectedItem()).trim();
             String speciesText = txtSpecies.getText().trim();
             String breedText = txtBreed.getText().trim();
             String remarksText = txtRemarks.getText().trim();
-            String clientIdText = (String) cbClientID.getSelectedItem();
+            String clientIdText = ((String) cbClientID.getSelectedItem()).trim();
 
             // Validate
             String errorMsg = validatePatientInput(idText, nameText, dobText, genderText, speciesText, breedText, clientIdText);
@@ -315,7 +315,7 @@ public class PatientManager extends JPanel {
                     genderText,
                     speciesText,
                     breedText,
-                    remarksText,
+                    txtRemarks.getText().trim(),
                     clientIdText
             );
 
